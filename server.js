80df8c7be3960dca76464df168af4e9ccd0342db
Kirannegi02 <negiaaradhya02@gmail.com>
2024-01-16 13:58:36 +0530

    Changes done

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
   console.log(req.files)
-  await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+  let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
   res.redirect(`http://localhost:3000/static/${d}.pdf`)
   // res.send({data: req.files})
 })
